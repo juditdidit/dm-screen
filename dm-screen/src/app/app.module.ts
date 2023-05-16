b2dea085f51419b1b02229b64cbde80b5c7bd095
Judit <hello@juditdidit.com>
2023-05-16 13:50:56 -0700

    kofi

--- dm-screen/src/app/app.module.ts
+++ dm-screen/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { InitiativeModule } from './sections/initiative/initiative.module';
 import { MonstersModule } from './sections/monsters/monsters.module';
 import { IconsModule } from './icons/icons.module';
+import { KofiComponent } from './kofi/kofi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KofiComponent
   ],
   imports: [
     BrowserModule,
