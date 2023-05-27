6118c0fab38a4949b05612dda3f4956de880194e
Judit <hello@juditdidit.com>
2023-05-27 08:13:56 -0700

    manual toggle switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+    align: 'vertical' | 'horizontal' = 'vertical';
+
+    toggleLayout(layout: 'vertical' | 'horizontal'): void {
+        this.align = layout;
+    }
+}
